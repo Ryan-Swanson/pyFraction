45ed16a4015c07b849340850369c5f3704c801dd
Ryan-Swanson <67406108+Ryan-Swanson@users.noreply.github.com>
2020-12-20 17:08:49 -0800

    Update main.py
    Added addition operator

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ class fraction:
         self.num = num
         self.den = den
 
+    def __add__(self, rhs):
+        self.simplify()
+        rhs.simplify()
+        gcf = self.den * rhs.den
+        self.num *= rhs.den
+        rhs.num *= self.den
+        self.den = gcf
+        rhs.den = gcf
+        self.num += rhs.num
+        self.simplify()
+        return self
+
     def simplify(self):
-        if self.num >= self.den:
+        if self.num == self.den:
+            self.num = 1
+            self.den = 1
+            return self
+
+        if  self.num >= self.den:
             limit = self.num
         else:
             limit = self.den
@@ -20,4 +37,7 @@ class fraction:
 
 
 f = fraction(int(input("Enter the numerator:")),int(input("Enter the denominator")))
+d = fraction(1,2)
+f = f + d
+
 f.printFraction()
